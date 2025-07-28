d5bbb4db2430f542f1bd951b8e63ae417b171538
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-28 02:11:10 +0200

    Basic product catalog created with product list and product detail views

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -11,6 +12,11 @@ class Category(models.Model):
         ]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse(
+            'shop:product_list_by_category', args=[self.slug]
+        )
 
     def __str__(self):
         return self.name
@@ -40,6 +46,9 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
 
     def __str__(self):
         return self.name
