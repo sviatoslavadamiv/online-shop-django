69660be8ea2e2fa8fffeb77790f6cb9f11cadcdf
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-29 14:50:27 +0200

    Generating PDF invoice dynamically functionality implemented

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -50,6 +50,11 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = (
@@ -64,6 +69,7 @@ class OrderAdmin(admin.ModelAdmin):
         "created",
         "updated",
         order_detail,
+        order_pdf,
     )
     list_filter = ('paid', 'created', 'updated')
     inlines = (OrderItemInline,)
--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,3 +1,9 @@
+from http.client import responses
+
+import weasyprint
+from django.contrib.staticfiles import finders
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from django.contrib.admin.views.decorators import staff_member_required
 from cart.cart import Cart
 from django.shortcuts import get_object_or_404, redirect, render
@@ -46,3 +52,15 @@ def admin_order_detail(request, order_id):
             'order': order,
         }
     )
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(
+        response,
+        stylesheets=[weasyprint.CSS(finders.find('css/pdf.css'))]
+    )
+    return response
