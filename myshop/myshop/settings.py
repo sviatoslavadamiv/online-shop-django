0743dd329aed6d86db752e990b1ff0fdffdbb6ab
sviatoslav <svatik5.adamiv@gmail.com>
2025-07-30 11:15:07 +0200

    New appliction named coupons created

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'cart.apps.CartConfig',
+    'coupons.apps.CouponsConfig',
     'orders.apps.OrdersConfig',
     'payment.apps.PaymentConfig',
     'shop.apps.ShopConfig',
